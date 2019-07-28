2852c0c069d3052e98cd19d6505e4ee14e6566ed
mirka1990 <glisic.mirjana90@gmail.com>
2019-07-28 19:29:24 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+function style(){
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir:'./'
+        }
+    });
+    gulp.watch('./scss/**/*.scss',style)
+    gulp.watch('./**/*.html').on('change',browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change',browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
+// gulp.task('sass',function(){
+//     return gulp.src('./scss/**/*.scss')
+//         .pipe(sass())
+//         .pipe(gulp.dest('./css'))
+
+// });
